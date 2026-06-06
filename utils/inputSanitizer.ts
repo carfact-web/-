5da5d2615b9e508641fb014f0817caafd2ae0ced
박신 <sin@bagsin-ui-Macmini.local>
2026-06-06 11:41:07 +0900

    Add multiline input sanitizer export

--- utils/inputSanitizer.ts
+++ utils/inputSanitizer.ts
@@ -1,6 +1,13 @@
 export const sanitizeUserText = (value: string) =>
   value.replace(/[<>]/g, "").replace(/[\u0000-\u001F\u007F]/g, "").trim();
 
+export const sanitizeMultilineUserText = (value: string) =>
+  value
+    .replace(/[<>]/g, "")
+    .replace(/\r\n?/g, "\n")
+    .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/g, "")
+    .trim();
+
 export const sanitizeVehiclePlateNumber = (value: string) =>
   value
     .replace(/[^0-9ㄱ-ㅎ가-힣]/g, "")
